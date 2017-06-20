fc1a55b6e28a0d30e5cd69d199ab24aa459c3572
Michael Chen Wang <michael.cw02@hotmail.com>
2017-06-19 19:22:45 -0600

    changed folder name

---
+++ 01-Basics/11-review.py
@@ -0,0 +1,24 @@
+
+# this is not a good practice in some cases
+entrada_1 = 'Nombre'
+if entrada_1 == 'Nombre':
+    print('Bad practice')
+else: 
+    print('What')
+
+# this is a good practice
+if entrada_1.lower() == 'NOmbre'.lower():
+    print('Good Practice')
+else 
+    print('Not working')
+
+
+for letter in 'UNIVERSIDAD'
+    print(letter)
+
+suma = 0
+for i in range(0, 10):
+    suma += i
+
+
+print('End!')
